e3d466d204278ac609a99cd2428ae465cb9f6c50
Mohit Verma <mohitverma2323@gmail.com>
2016-04-13 20:27:42 +0530

    some minor changes

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import {MongoClient} from 'mongodb';
 
 let app = express();
 
 app.use(express.static('public'));
 
 app.listen(3000);
+
+MongoClient.connect(process.env.MONGO_URL,(err, database) => {
+    
+    if(err) throw err;
+    
+    database.collection("links").find({}).toArray((err, links)=>{
+        
+        console.log(links);
+    });
+});
